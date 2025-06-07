e998fe479398be782ce3b4e154379625f778ff01
NahuelPerrone85 <nahuelperrone85@gmail.com>
2025-06-07 11:58:46 +0000

    Falta funcion

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,26 +3,28 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
+//create your first componen
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+function Home() {
+const  {clickColor, setClickColor} = React.useState("red");
+
+	return (
+		<div className="container rounded-5 d-flex justify-content-center bg-black w-25">
+  		<div className="row row-cols-2 m-4 d-flex flex-column align-items-center">
+   		 <div 
+		 onClick={() => setClickColor("danger")}
+		 className={"col bg-danger m-1 rounded-circle  p-5 m-3" + 
+		 (clickColor ===	"danger" ? "flash" : " " )}> </div>
+   		 <div 
+		 onClick={() => setClickColor("warning")}
+		 className={"col bg-warning m-1 rounded-circle  p-5 m-3" +
+		(clickColor ===	"warning" ? "flash" : " " )}></div>
+   		 <div 
+		 onClick={() => setClickColor("success")}
+		 className={"col bg-success m-1 rounded-circle  p-5 m-3" + 
+		 (clickColor ===	"success" ? "flash" : " " )}> </div>
+		</div>
 		</div>
 	);
 };
-
 export default Home;
